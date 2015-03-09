f50ee10b24f00496208f69c39a2cfe262fe0971f
Juan Obando <juanchopx2@gmail.com>
2015-03-08 22:56:52 -0400

    Making work Ruby SASS lib.

--- gulpfile.js
+++ gulpfile.js
@@ -6,21 +6,15 @@ var $ = require('gulp-load-plugins')();
 var browserSync = require('browser-sync');
 var reload = browserSync.reload;
 
-gulp.task('styles', function () {
-  return gulp.src('app/styles/main.scss')
-    .pipe($.sourcemaps.init())
-    .pipe($.sass({
-      outputStyle: 'nested', // libsass doesn't support expanded yet
-      precision: 10,
-      includePaths: ['.'],
-      onError: console.error.bind(console, 'Sass error:')
-    }))
-    .pipe($.postcss([
-      require('autoprefixer-core')({browsers: ['last 1 version']})
-    ]))
+gulp.task('styles', function() {
+  return $.rubySass('app/styles', {
+      sourcemap: true
+    })
+    .on('error', function (err) {
+      console.log(err.message);
+    })
     .pipe($.sourcemaps.write())
-    .pipe(gulp.dest('.tmp/styles'))
-    .pipe(reload({stream: true}));
+    .pipe(gulp.dest('.tmp/styles'));
 });
 
 gulp.task('jshint', function () {
